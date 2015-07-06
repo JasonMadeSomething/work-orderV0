2ba708ee1c211f8d378722b7361310746942c486
Jason Teitelman <super.soviet.jesus@gmail.com>
2015-07-06 12:30:27 -0400

    De-lint client and permit form (some)

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,8 +1,8 @@
 class Client < ActiveRecord::Base
-    has_many :contacts
-    has_many :addresses, :through  => :contacts
-    has_many :permits
-    validates :clientnumber, format: { with: /\A\d{3}\z/ },
-                             length: { is: 3 }
-    has_many :work_orders
+  has_many :contacts
+  has_many :addresses, through: :contacts
+  has_many :permits
+  validates :clientnumber, format: { with: /\A\d{3}\z/ },
+                                     length: { is: 3 }
+  has_many :work_orders
 end
