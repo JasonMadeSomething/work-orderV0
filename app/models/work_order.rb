f97a78cf0fbcdab166de2aa17e0c6c7d67dd2b1c
Jason Teitelman <super.soviet.jesus@gmail.com>
2015-07-06 11:14:48 -0400

    delinted some of work order model

--- app/models/work_order.rb
+++ app/models/work_order.rb
@@ -14,58 +14,56 @@ class WorkOrder < ActiveRecord::Base
   validates_presence_of :status_id
   validates_format_of :number,  with: /(?:00[1-9]|0[1-9]\d|[1-9]\d\d)\d{2}(?:0[1-9]|1[0-2])-(?:00[1-9]|0[1-9]\d|[1-9]\d\d)/
   validates_format_of :monthlySequenceNumber, with: /(?:00[1-9]|0[1-9]\d|[1-9]\d\d)/
-  validates :printing_instructions, presence: true, if: :has_printing_componant?
-  validates :presort_information, presence: true, if: :has_mailing_componant?
+  validates :printing_instructions, presence: true, if: :printing_componant?
+  validates :presort_information, presence: true, if: :mailing_componant?
   accepts_nested_attributes_for :presort_information, :printing_instructions, :production_details
-  
+
   before_validation :set_monthly_sequence_number, :set_workorder_number, on: :create, unless: @number
-  
+
   def set_workorder_number
-      set_monthly_sequence_number
-      self.number = "#{self.client.clientnumber}#{month_and_year_string}-#{self.monthlySequenceNumber}" if client
+    set_monthly_sequence_number
+    self.number = "#{ self.client.clientnumber }#{ month_and_year_string }-#{ self.monthlySequenceNumber }" if client
   end
-  
+
   def display_number
-     self.number[0..2] + "-" + self.number[5..6] + "-" + self.number[-3,3]
+    self.number[0..2] + '-' + self.number[5..6] + '-' + self.number[-3, 3]
   end
-  
+
   def self.schedule
-    WorkOrder.joins(:status).where(statuses: {description: "Active"}).order(:dueDate)
+    WorkOrder.joins(:status).where(statuses: { description: 'Active' }).order(:dueDate)
   end
-  
+
   def active?
-    if self.status.description == "Active"
+    if self.status.description == 'Active'
       true
     else
       false
     end
   end
-  
-  def has_mailing_componant?
+
+  def mailing_componant?
     project_type.mailing if project_type
   end
-  
-  def has_printing_componant?
+
+  def printing_componant?
     project_type.printing if project_type
   end
-  
+
   private
 
-    
     def set_monthly_sequence_number
-      self.monthlySequenceNumber = get_next_sequence_number
+      self.monthlySequenceNumber = next_sequence_number
     end
-    
+
     def created_this_month
       WorkOrder.where(:created_at => Time.now.beginning_of_month..Time.now.end_of_month).count
     end
-    
-    def get_next_sequence_number
-      "%03d" % (created_this_month + 1)
+
+    def next_sequence_number
+      '%03d' % (created_this_month + 1)
     end
-    
+
     def month_and_year_string
-      "#{Date.today.year.to_s[-2,2]}#{"%02d" % Date.today.month.to_s}"
+      "#{Date.today.year.to_s[-2, 2]}#{'%02d' % Date.today.month.to_s}"
     end
-    
 end
