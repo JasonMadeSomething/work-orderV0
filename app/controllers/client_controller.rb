ab0f2b4a2a5e5a7573620f421063239192359fe6
Jason Teitelman <super.soviet.jesus@gmail.com>
2015-07-06 18:10:24 -0400

    update workspace, more lint

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,3 +1,5 @@
+# Controller for clients. Need only bare minimum given
+# I don't plan to do anything complicated with clients for now
 class ClientController < ApplicationController
   def index
     @client = Client.all
@@ -7,6 +9,7 @@ class ClientController < ApplicationController
     @workorders = WorkOrder.schedule.where(client_id: params[:client_id])
     render 'work_orders/schedule'
   end
+
   def archive
     @workorders = WorkOrder.where(client_id: params[:client_id]).order(:created_at).reverse_order
     render 'work_orders/archive'
