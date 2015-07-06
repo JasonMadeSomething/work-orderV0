97fcfe9672e32370f5cb674226a3f32eb3341fdc
Jason Teitelman <super.soviet.jesus@gmail.com>
2015-07-06 15:52:07 -0400

    de-linting and Permit Form edit
    de-lint client controller, Remove submit from permit form

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,17 +1,14 @@
 class ClientController < ApplicationController
-    
-    def index
-       @client = Client.all 
-    end
-    
-    def schedule
-        @workorders = WorkOrder.schedule.where(client_id: params[:client_id])
-        render 'work_orders/schedule'
-    end
-    
-    def archive
-        @workorders = WorkOrder.where(client_id: params[:client_id]).order(:created_at).reverse_order
-        render 'work_orders/archive'
-    end
-    
+  def index
+    @client = Client.all
+  end
+
+  def schedule
+    @workorders = WorkOrder.schedule.where(client_id: params[:client_id])
+    render 'work_orders/schedule'
+  end
+  def archive
+    @workorders = WorkOrder.where(client_id: params[:client_id]).order(:created_at).reverse_order
+    render 'work_orders/archive'
+  end
 end
