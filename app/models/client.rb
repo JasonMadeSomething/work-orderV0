3c99a197cc1059c81332a613c860713a8495ed92
Jason Teitelman <super.soviet.jesus@gmail.com>
2015-07-06 12:31:09 -0400

    Align validation hash

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,6 +3,6 @@ class Client < ActiveRecord::Base
   has_many :addresses, through: :contacts
   has_many :permits
   validates :clientnumber, format: { with: /\A\d{3}\z/ },
-                                     length: { is: 3 }
+                                    length: { is: 3 }
   has_many :work_orders
 end
