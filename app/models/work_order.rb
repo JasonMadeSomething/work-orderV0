3c72ad86bee9fd814a32993330b212f37d5cb64e
Jason Teitelman <super.soviet.jesus@gmail.com>
2015-07-07 15:55:57 -0400

    Line length and new hash syntax

--- app/models/work_order.rb
+++ app/models/work_order.rb
@@ -12,13 +12,19 @@ class WorkOrder < ActiveRecord::Base
   validates_presence_of :project_type_id
   validates_presence_of :monthlySequenceNumber
   validates_presence_of :status_id
-  validates_format_of :number,  with: /(?:00[1-9]|0[1-9]\d|[1-9]\d\d)\d{2}(?:0[1-9]|1[0-2])-(?:00[1-9]|0[1-9]\d|[1-9]\d\d)/
-  validates_format_of :monthlySequenceNumber, with: /(?:00[1-9]|0[1-9]\d|[1-9]\d\d)/
+  validates_format_of :number, with: /(?:00[1-9]|0[1-9]\d|[1-9]\d\d)\d{2}(?:0[1-9]|1[0-2])-(?:00[1-9]|0[1-9]\d|[1-9]\d\d)/
+  validates_format_of :monthlySequenceNumber,
+                      with: /(?:00[1-9]|0[1-9]\d|[1-9]\d\d)/
   validates :printing_instructions, presence: true, if: :printing_componant?
   validates :presort_information, presence: true, if: :mailing_componant?
-  accepts_nested_attributes_for :presort_information, :printing_instructions, :production_details
+  accepts_nested_attributes_for :presort_information,
+                                :printing_instructions,
+                                :production_details
 
-  before_validation :set_monthly_sequence_number, :set_workorder_number, on: :create, unless: @number
+  before_validation :set_monthly_sequence_number,
+                    :set_workorder_number,
+                    on: :create,
+                    unless: @number
 
   def set_workorder_number
     set_monthly_sequence_number
@@ -56,7 +62,7 @@ class WorkOrder < ActiveRecord::Base
     end
 
     def created_this_month
-      WorkOrder.where(:created_at => Time.now.beginning_of_month..Time.now.end_of_month).count
+      WorkOrder.where(created_at: Time.now.beginning_of_month..Time.now.end_of_month).count
     end
 
     def next_sequence_number
